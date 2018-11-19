b86876f6bc1b05f73f184915243a520c02c53f9b
Basel <z.basel324@gmail.com>
2018-11-19 18:02:57 +0200

    Update Camera.cpp

--- Viewer/src/Camera.cpp
+++ Viewer/src/Camera.cpp
@@ -5,7 +5,17 @@
 #include <iostream>
 
 Camera::Camera(const glm::vec4& eye, const glm::vec4& at, const glm::vec4& up) :
-	zoom(1.0)
+	zoom(1.0),
+	viewTransformation({   1, 0, 0, 0,
+						  0, 1, 0, 0,
+						  0, 0, 1, 0,
+						  0, 0, 0, 1 }),
+	projectionTransformation(
+						  { 1, 0, 0, 0,
+							0, 1, 0, 0,
+							0, 0, 1, 0,
+							0, 0, 0, 1 })
+
 {
 	SetCameraLookAt(eye, at, up);
 }
@@ -18,40 +28,40 @@ void Camera::SetCameraLookAt(const glm::vec3& eye, const glm::vec3& at, const gl
 {
 	glm::vec3 z = normalize(eye - at);
 	glm::vec3 x = normalize(glm::cross(up, z));
-	glm::vec3 y = normalize(glm::cross(z, x));
+	glm::vec3 y = normalize(glm::cross(x, z));
 	glm::vec4 z4(z[0], z[1], z[2], 0);
 	glm::vec4 x4(x[0], x[1], x[2], 0);
 	glm::vec4 y4(y[0], y[1], y[2], 0);
 	glm::vec4 t(0, 0, 0, 1);
 	glm::mat4 c = glm::mat4(x4, y4, z4, t);
 	glm::mat4x4 translationMatrix
-	{ 1	,	0	,	0	,	    -eye[0],
-		0	,	1	,	0	,	-eye[1],
-		0	,	0	,	1	,	-eye[2],
+	{ 1	,	0	,	0	,	    eye[0],
+		0	,	1	,	0	,	eye[1],
+		0	,	0	,	1	,	eye[2],
 		0	,	0	,	0	,	1 };
 
-	viewTransformation = c * translationMatrix;
+	viewTransformation = c * glm::transpose(translationMatrix);
 }
 
 void Camera::SetOrthographicProjection(const float left, const float right, const float bottom, const float top, const float near, const float far)
 {
-	glm::mat4x4 C
-	{ 2.0 / (right - left)		,	0						,		0					,	-1.0*((right + left) / (right - left)),
-		0						,	2.0 / (top - bottom)	,		0					,	-1.0*((top + bottom) / (top - bottom)),
-		0						,	0						,		2.0 / (near - far)	,	-1.0*((far + near) / (far - near)),
-		0						,	0						,		0					,	1,
-	};
+	glm::mat4x4 C(
+	{ 2.0 / (right - left)		,	0.0						,		0.0					,	-1.0*((right + left) / (right - left)),
+		0.0						,	2.0 / (top - bottom)	,		0.0					,	-1.0*((top + bottom) / (top - bottom)),
+		0.0						,	0.0						,		2.0 / (near - far)	,	-1.0*((far + near) / (far - near)),
+		0.0						,	0.0						,		0.0					,	1.0,
+	});
 	projectionTransformation = C;
 }
 
 void Camera::SetPerspectiveProjection(const float left, const float right, const float bottom, const float top, const float near, const float far)
 {
-	glm::mat4x4 C
+	glm::mat4x4 C(
 	{ (2.0 * near) / (right - left)		,	0								,		(right + left) / (right - left)		,	0,
 		0								,	(2.0 * near) / (top - bottom)	,		 (top + bottom) / (top - bottom)	,	0,
 		0								,	0								,		-1 * (far + near) / (far - near)	,	-1 * ((2 * far * near) / (far - near)),
 		0								,	0								,	    -1									,	0,
-	};
+	});
 	projectionTransformation = C;
 }
 
@@ -76,4 +86,9 @@ void Camera::SetZoom(const float zoom)
 glm::mat4x4 Camera::getViewTransformationInverse() const
 {
 	return glm::inverse(viewTransformation);
+}
+
+glm::mat4x4 Camera::getProjectionTransformation() const
+{
+	return projectionTransformation;
 }
