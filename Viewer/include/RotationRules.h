48a948ba52cd6f166efd12adaa9bb60b86b5ee3f
Elie Abboud <eliabboud1000@gmail.com>
2018-11-22 20:27:58 +0200

    Update RotationRules.h
    ++Documentation:
    * Added documentation to help understand the usage of the functions.

--- Viewer/include/RotationRules.h
+++ Viewer/include/RotationRules.h
@@ -17,15 +17,22 @@ private:
 	float _zAngleDegrees, _zAngleRadians;
 	//TODO: array that describes the order 
 
+	/*converts radians to degrees*/
 	float radiansToDegree(float radian);
+	/*converts degrees to radians*/
 	float degreeToRadians(float degree);
+	/*updates the Axis axis to rotate with angle degree */
 	void updateRotation(Axis axis, float angle, AngleUnits unit);
-	void updateDegreeCoordinates(Axis axis, float angle);
+	/*updates angle value, assumes that it is passed a degree value */
+	void updateDegreeCoordinates(Axis axis, float degree);
 public:
 	RotationRules(float xAngle = 0.0f, float yAngle = 0.0f, float zAngle = 0.0f, AngleUnits unit = AngleUnits::DEGREES);
 	void setRotation(Axis axis, float angle, AngleUnits unit = AngleUnits::DEGREES);
+	/*sets rotation for each axis, when an angle is set to nullptr, it keeps it unchanged. */
 	void setRotationAll(float* xAngle, float* yAngle, float* zAngle, AngleUnits unit);
+	/*removes model's rotation on the axis axis.*/
 	void resetRotation(Axis axis);
+	/*removes model's rotation completely.*/
 	void resetRotation();
 	float getAngleX(AngleUnits unit) const;
 	float getAngleY(AngleUnits unit) const;
